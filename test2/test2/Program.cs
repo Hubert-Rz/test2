59505221d8a07904ab94961d9cc792955c471052
h_rzazewski <hubertr@poczta.fm>
2023-02-04 19:42:44 +0100

    zad domowe  dzień 4

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// zadanie domowe dzień 4
+
+string imie = "Ewa";
+bool kobieta = false;
+int wiek = 33;
+
+if (kobieta == true) //kobieta
+{
+    if (imie == "Ewa" && wiek == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (imie != "Ewa" && wiek < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+else //mężczyzna
+{
+    if (wiek < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    } 
+    else if ( imie=="Ewa")
+    {
+        Console.WriteLine("Czy mężczyzna może mieć na imię Ewa ?");
+    }
+}

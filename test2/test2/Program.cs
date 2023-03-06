dfed119207a654f51fb9a532bd75eb1a96a3a045
h_rzazewski <hubertr@poczta.fm>
2023-03-06 20:39:44 +0100

    Zadanie domowe dzień 11

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -11,10 +11,28 @@ using test2;
     emp.AddGrade(2);
     emp.AddGrade('1');
     emp.AddGrade('x');
-    var statistics=emp.GetStatistics();
-    Console.WriteLine($"Average: {statistics.Average:N2}");
-    Console.WriteLine($"Min: {statistics.Min}");
-    Console.WriteLine($"Max: {statistics.Max}");
+    //var statistics = emp.GetStatistics();
+    var statistics1 = emp.GetStatisticsWithForEach();
+    var statistics2 = emp.GetStatisticsWithFor();
+    var statistics3 = emp.GetStatisticsWithDoWhile();
+    var statistics4 = emp.GetStatisticsWithWhile();
+
+    Console.WriteLine("----------------GetStatisticsWithForEach----------------");
+    Console.WriteLine($"Average: {statistics1.Average:N2}");
+    Console.WriteLine($"Min: {statistics1.Min}");
+    Console.WriteLine($"Max: {statistics1.Max}");
+    Console.WriteLine("-----------------GetStatisticsWithFor---------------");
+    Console.WriteLine($"Average: {statistics2.Average:N2}");
+    Console.WriteLine($"Min: {statistics2.Min}");
+    Console.WriteLine($"Max: {statistics2.Max}");
+    Console.WriteLine("----------------GetStatisticsWithDoWhile----------------");
+    Console.WriteLine($"Average: {statistics3.Average:N2}");
+    Console.WriteLine($"Min: {statistics3.Min}");
+    Console.WriteLine($"Max: {statistics3.Max}");
+    Console.WriteLine("----------------GetStatisticsWithWhile----------------");
+    Console.WriteLine($"Average: {statistics4.Average:N2}");
+    Console.WriteLine($"Min: {statistics4.Min}");
+    Console.WriteLine($"Max: {statistics4.Max}");
 
 
 }
